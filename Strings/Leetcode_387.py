645ff89a92812990512526e9f8dd13f09928df8f
Shivam1848 <kashokagrawal1848@gmail.com>
2024-11-30 10:57:10 +0530

    Nov 30

---
+++ Strings/Leetcode_387.py
@@ -0,0 +1,17 @@
+from collections import Counter
+s = "loveleetcode"
+
+# ---------TLE----------
+# for i in range(0,len(s)):
+#     count = s.count(s[i])
+#     if count == 1:
+#         print(i)
+#         break
+
+# Best Complexity
+# beat - 97%
+a = Counter(s)
+for i in a:
+    if a[i] == 1:
+        print(s.index(i))
+        break
--- Strings/Leetcode_459.py
+++ Strings/Leetcode_459.py
@@ -1,16 +1,7 @@
-s = "abaababaab"
-window = 0
+s = "aba"
 
-for i in range(0,len(s)//2):
-    if s[:i+1] == s[-(i+1):]:
-        window = i+1
-print(window)
-
-newStr = s[:window]
-for i in range(0,len(s),window):
-    if s[i:i+window] != newStr:
-        print(False)
-        break
-
-if window >= len(s)//2:
+if not s:
     print(False)
+
+ss = (s+s)[1:-1]
+print(ss.find(s) != -1)
