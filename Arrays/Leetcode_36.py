c433bc7d00a93a2aad9ee44e40011c0fc1af076d
Shivam1848 <kashokagrawal1848@gmail.com>
2024-12-06 06:44:18 +0530

    Dec 6

--- Arrays/Leetcode_36.py
+++ Arrays/Leetcode_36.py
@@ -30,15 +30,15 @@
     Example 2:
         Input: 
             board = 
-                [["8","3",".",".","7",".",".",".","."]
-                ,["6",".",".","1","9","5",".",".","."]
-                ,[".","9","8",".",".",".",".","6","."]
-                ,["8",".",".",".","6",".",".",".","3"]
-                ,["4",".",".","8",".","3",".",".","1"]
-                ,["7",".",".",".","2",".",".",".","6"]
-                ,[".","6",".",".",".",".","2","8","."]
-                ,[".",".",".","4","1","9",".",".","5"]
-                ,[".",".",".",".","8",".",".","7","9"]]
+                [[".",".",".",".","5",".",".","1","."]
+                ,[".","4",".","3",".",".",".",".","."]
+                ,[".",".",".",".",".","3",".",".","1"]
+                ,["8",".",".",".",".",".",".","2","."]
+                ,[".",".","2",".","7",".",".",".","."]
+                ,[".","1","5",".",".",".",".",".","."]
+                ,[".",".",".",".",".","2",".",".","."]
+                ,[".","2",".","9",".",".",".",".","."]
+                ,[".",".","4",".",".",".",".",".","."]
         Output: 
             false
         Explanation: 
@@ -48,25 +48,25 @@
 '''
 
 def isValidSudoku(board):
-    # for i in range(9):
-    #     rowNumbers = ["1","2","3","4","5","6","7","8","9"]
-    #     colNumbers = ["1","2","3","4","5","6","7","8","9"]
-    #     for j in range(9):
-    #         if board[i][j] in rowNumbers: #board[i][j] != ".":
-    #             rowNumbers.remove(board[i][j])
-    #             # print(board[i][j])
-    #         elif board[i][j] == ".":
-    #             pass
-    #         else:
-    #             return False
+    for i in range(9):
+        rowNumbers = ["1","2","3","4","5","6","7","8","9"]
+        colNumbers = ["1","2","3","4","5","6","7","8","9"]
+        for j in range(9):
+            if board[i][j] in rowNumbers: #board[i][j] != ".":
+                rowNumbers.remove(board[i][j])
+                # print(board[i][j])
+            elif board[i][j] == ".":
+                pass
+            else:
+                return False
             
-    #         if board[j][i] in colNumbers: #board[i][j] != ".":
-    #             colNumbers.remove(board[j][i])
-    #             print(board[j][i])
-    #         elif board[j][i] == ".":
-    #             pass
-    #         else:
-    #             return False
+            if board[j][i] in colNumbers: #board[i][j] != ".":
+                colNumbers.remove(board[j][i])
+                print(board[j][i])
+            elif board[j][i] == ".":
+                pass
+            else:
+                return False
     boxNumbers = ["1","2","3","4","5","6","7","8","9"]
     for i in range(3):
         for j in range(3):
@@ -80,6 +80,76 @@ def isValidSudoku(board):
     boxNumbers = ["1","2","3","4","5","6","7","8","9"]
     for i in range(3):
         for j in range(3,6):
+            if board[i][j] in boxNumbers: #board[i][j] != ".":
+                boxNumbers.remove(board[i][j])
+                print(board[i][j])
+            elif board[i][j] == ".":
+                pass
+            else:
+                return False
+    boxNumbers = ["1","2","3","4","5","6","7","8","9"]
+    for i in range(3):
+        for j in range(6,9):
+            if board[i][j] in boxNumbers: #board[i][j] != ".":
+                boxNumbers.remove(board[i][j])
+                print(board[i][j])
+            elif board[i][j] == ".":
+                pass
+            else:
+                return False
+    boxNumbers = ["1","2","3","4","5","6","7","8","9"]
+    for i in range(3,6):
+        for j in range(3):
+            if board[i][j] in boxNumbers: #board[i][j] != ".":
+                boxNumbers.remove(board[i][j])
+                print(board[i][j])
+            elif board[i][j] == ".":
+                pass
+            else:
+                return False
+    boxNumbers = ["1","2","3","4","5","6","7","8","9"]
+    for i in range(3,6):
+        for j in range(3,6):
+            if board[i][j] in boxNumbers: #board[i][j] != ".":
+                boxNumbers.remove(board[i][j])
+                print(board[i][j])
+            elif board[i][j] == ".":
+                pass
+            else:
+                return False
+    boxNumbers = ["1","2","3","4","5","6","7","8","9"]
+    for i in range(3,6):
+        for j in range(6,9):
+            if board[i][j] in boxNumbers: #board[i][j] != ".":
+                boxNumbers.remove(board[i][j])
+                print(board[i][j])
+            elif board[i][j] == ".":
+                pass
+            else:
+                return False
+    boxNumbers = ["1","2","3","4","5","6","7","8","9"]
+    for i in range(6,9):
+        for j in range(3):
+            if board[i][j] in boxNumbers: #board[i][j] != ".":
+                boxNumbers.remove(board[i][j])
+                print(board[i][j])
+            elif board[i][j] == ".":
+                pass
+            else:
+                return False
+    boxNumbers = ["1","2","3","4","5","6","7","8","9"]
+    for i in range(6,9):
+        for j in range(3,6):
+            if board[i][j] in boxNumbers: #board[i][j] != ".":
+                boxNumbers.remove(board[i][j])
+                print(board[i][j])
+            elif board[i][j] == ".":
+                pass
+            else:
+                return False
+    boxNumbers = ["1","2","3","4","5","6","7","8","9"]
+    for i in range(6,9):
+        for j in range(6,9):
             if board[i][j] in boxNumbers: #board[i][j] != ".":
                 boxNumbers.remove(board[i][j])
                 print(board[i][j])
